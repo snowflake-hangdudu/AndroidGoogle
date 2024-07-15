83f63041f7fcedcd03843d273230df410d0e461f
hangdudu <15551359775@163.com>
2024-07-15 19:26:42 +0800

    1

--- ArtSpace/app/src/main/java/com/example/artspace/MainActivity.java
+++ ArtSpace/app/src/main/java/com/example/artspace/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.artspace;
 
+import android.widget.Button;
+import android.widget.ImageView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+     Button previous = findViewById(R.id.previous);
+     Button next = findViewById(R.id.next);
+
+        ImageView art = findViewById(R.id.art);
+
+     next.setOnClickListener(v -> {
+         art.setImageResource(R.drawable.art2);
+     });
+
+     previous.setOnClickListener(v -> {
+         art.setImageResource(R.drawable.art);
+     });
+
+
+
+
     }
 }
