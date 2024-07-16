e78fcea9705dfd0be1a7dbf3b18efdb6f65732f0
hangdudu <15551359775@163.com>
2024-07-17 00:58:59 +0800

    1

---
+++ 30DaysOfWellness/app/src/main/java/com/example/a30daysofwellness/adapter/Day30Adapater.java
@@ -0,0 +1,5 @@
+package com.example.a30daysofwellness.adapter;
+
+public class Day30Adapater   {
+
+}
---
+++ 30DaysOfWellness/app/src/main/java/com/example/a30daysofwellness/day30/Day30.java
@@ -0,0 +1,30 @@
+package com.example.a30daysofwellness.day30;
+
+public class Day30 {
+    int imgid;
+    String name;
+    String desc;
+    String longdesc;
+
+
+
+    public Day30(int imgid, String name, String desc, String longdesc) {
+        this.imgid = imgid;
+        this.name = name;
+        this.desc = desc;
+        this.longdesc = longdesc;
+    };
+
+    public int getImgid() {
+        return imgid;
+    }
+    public String getName() {
+        return name;
+    }
+    public String getDesc() {
+        return desc;
+    }
+    public String getLongdesc() {
+        return longdesc;
+    }
+}
